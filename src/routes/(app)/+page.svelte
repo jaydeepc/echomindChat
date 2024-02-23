cb514e9cedaef272c71613d71ca7bdde19455757
Aldrin Jenson <aldrin.jenson@thoughtworks.com>
2024-02-23 16:10:43 +0530

    remove unnecessary console logs

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -396,7 +396,6 @@
 
 					for (const line of lines) {
 						if (line !== '') {
-							console.log(line);
 							let data = JSON.parse(line);
 
 							if ('detail' in data) {
@@ -607,13 +606,11 @@
 
 					for (const line of lines) {
 						if (line !== '') {
-							console.log(line);
 							if (line === 'data: [DONE]') {
 								responseMessage.done = true;
 								messages = messages;
 							} else {
 								let data = JSON.parse(line.replace(/^data: /, ''));
-								console.log(data);
 
 								if (responseMessage.content == '' && data.choices[0].delta.content == '\n') {
 									continue;
