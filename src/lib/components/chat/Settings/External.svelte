1039f70114a8918463586887e0bda270826e2825
Aldrin Jenson <aldrin.jenson@thoughtworks.com>
2024-02-23 09:45:00 +0530

    Add UI for Azure OpenAI setting

--- src/lib/components/chat/Settings/External.svelte
+++ src/lib/components/chat/Settings/External.svelte
@@ -10,9 +10,19 @@
 	let OPENAI_API_KEY = '';
 	let OPENAI_API_BASE_URL = '';
 
+	// Azure OpenAI
+	let AZURE_DEPLOYMENT_NAME = '';
+	let AZURE_MODEL_NAME = '';
+	let AZURE_OPENAI_VERSION = '';
+	let SHOULD_USE_AZURE = false;
+
 	const updateOpenAIHandler = async () => {
 		OPENAI_API_BASE_URL = await updateOpenAIUrl(localStorage.token, OPENAI_API_BASE_URL);
 		OPENAI_API_KEY = await updateOpenAIKey(localStorage.token, OPENAI_API_KEY);
+		localStorage.setItem('use_azure', SHOULD_USE_AZURE.toString());
+		localStorage.setItem('azure_openai_version', AZURE_OPENAI_VERSION);
+		localStorage.setItem('azure_deployment_name', AZURE_DEPLOYMENT_NAME);
+		localStorage.setItem('azure_model_name', AZURE_MODEL_NAME);
 
 		await models.set(await getModels());
 	};
@@ -21,8 +31,21 @@
 		if ($user.role === 'admin') {
 			OPENAI_API_BASE_URL = await getOpenAIUrl(localStorage.token);
 			OPENAI_API_KEY = await getOpenAIKey(localStorage.token);
+
+			AZURE_OPENAI_VERSION = localStorage.getItem('azure_openai_version') || '';
+			console.log(localStorage.getItem('use_azure'));
+			// should_use_azure should be true if in localstorage it says 'true' else false
+			SHOULD_USE_AZURE = localStorage.getItem('use_azure') === 'true';
+			// SHOULD_USE_AZURE = (localStorage.getItem('use_azure')) || false;
+			console.log({ SHOULD_USE_AZURE });
+			AZURE_DEPLOYMENT_NAME = localStorage.getItem('azure_deployment_name') || '';
+			AZURE_MODEL_NAME = localStorage.getItem('azure_model_name') || '';
 		}
 	});
+
+	const handleClick = (e) => {
+		SHOULD_USE_AZURE = e.target.checked;
+	};
 </script>
 
 <form
@@ -39,7 +62,7 @@
 >
 	<div class=" space-y-3">
 		<div>
-			<div class=" mb-2.5 text-sm font-medium">OpenAI API Key</div>
+			<div class=" mb-2.5 text-sm font-medium">OpenAI/Azure API Key</div>
 			<div class="flex w-full">
 				<div class="flex-1">
 					<input
@@ -58,7 +81,7 @@
 		<hr class=" dark:border-gray-700" />
 
 		<div>
-			<div class=" mb-2.5 text-sm font-medium">OpenAI API Base URL</div>
+			<div class=" mb-2.5 text-sm font-medium">OpenAI/Azure API Base URL</div>
 			<div class="flex w-full">
 				<div class="flex-1">
 					<input
@@ -73,6 +96,68 @@
 				WebUI will make requests to <span class=" text-gray-200">'{OPENAI_API_BASE_URL}/chat'</span>
 			</div>
 		</div>
+
+		<div class="flex space-x-2 text-sm">
+			<input
+				type="checkbox"
+				bind:checked={SHOULD_USE_AZURE}
+				on:click={handleClick}
+				id="azure-toggle"
+			/>
+			<label for="azure-toggle">Use Azure OpenAI</label>
+		</div>
+
+		{#if SHOULD_USE_AZURE}
+			<div>
+				<div class=" mb-2.5 text-sm font-medium">API Version</div>
+				<div class="flex w-full">
+					<div class="flex-1">
+						<input
+							class="w-full rounded py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-800 outline-none"
+							placeholder="eg: 2023-05-15"
+							bind:value={AZURE_OPENAI_VERSION}
+							autocomplete="off"
+							required
+						/>
+					</div>
+				</div>
+				<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">Azure OpenAI API version</div>
+			</div>
+
+			<div class="flex w-full">
+				<!-- Left column for Model Name -->
+				<div class="flex-1 pr-2">
+					<div class="mb-2.5 text-sm font-medium">Model Name (Optional)</div>
+					<div class="flex w-full">
+						<div class="flex-1">
+							<input
+								class="w-full rounded py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-800 outline-none"
+								placeholder="Enter Model Name"
+								bind:value={AZURE_MODEL_NAME}
+								autocomplete="off"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<!-- Right column for Deployment Name -->
+				<div class="flex-1 pl-2">
+					<div class="mb-2.5 text-sm font-medium">Deployment Name</div>
+					<div class="flex w-full">
+						<div class="flex-1">
+							<input
+								class="w-full rounded py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-800 outline-none"
+								placeholder="Enter Deployment Name"
+								bind:value={AZURE_DEPLOYMENT_NAME}
+								autocomplete="off"
+								required
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">eg: GPT 3.5 fine tuned</div>
+		{/if}
 	</div>
 
 	<div class="flex justify-end pt-3 text-sm font-medium">
